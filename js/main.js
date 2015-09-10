cf9518507dd3f5bbcbffda42e269389f90a492c6
Damien Hampton <damien.hampton@gmail.com>
2015-09-10 12:11:36 +0100

    Made open items close before opening new ones

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $("li").click(function(){
+	//if clicked open item, return
+	if($(this).hasClass("open")){
+		return
+	}
+	//if no item is open
+	if($("li.open").length <= 0){
+		$(this).addClass("open");
+		return;
+	}
+	//else close the open item, then open the selected one
+	$(this).addClass("waiting");
+	$("li.open").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+		$("li.waiting").addClass("open");
+		$("li.waiting").removeClass("waiting");
+	});
 	$("li.open").removeClass("open");
-	$(this).addClass("open");
 });
+
